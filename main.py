dfedabcb08fe54fc8bde893eee13655f1289c00d
Shachar Levy <shacharl663@gmail.com>
2024-01-17 10:29:01 +0200

    changes

--- main.py
+++ main.py
@@ -1,11 +1,126 @@
 import json
+import nltk
+from collections import defaultdict
+import string
+import pickle
+import os.path
+nltk.download('punkt')
+
+
+def split_text_to_sentences(file_path):
+    formatted_sentences = []
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            cleaned_line = line.translate(str.maketrans('', '', string.punctuation))
+            formatted_sentences.append(f"<s> {cleaned_line.strip()} </s>")
+
+    return formatted_sentences
+
+
+def get_words_and_bigrams_frequencies(sentences, lexicon_words):
+    words_freq = defaultdict(int)
+    bigrams_freq = defaultdict(dict)
+
+    for sentence in sentences:
+        for i, word in enumerate(sentence):
+            if word in lexicon_words:
+                words_freq[word] += 1
+            if i < len(sentence) - 1:
+                if sentence[i] in lexicon_words and sentence[i + 1] in lexicon_words:
+                    bigrams_freq[sentence[i]][sentence[i + 1]] += 1
+
+    return words_freq, bigrams_freq
+
+
+def get_2grams_statistics(corpus_path, lexicon_path):
+    bigrams_prob = defaultdict(dict)
+
+    with open(lexicon_path, 'r', encoding='utf-8') as lexicon_file:
+        lexicon_content = lexicon_file.read()
+
+    lexicon_words = set(lexicon_content.split())
+    lexicon_words.add('<s>')
+    lexicon_words.add('</s>')
+
+    sentences = split_text_to_sentences(corpus_path)
+    words_freq, bigrams_freq = get_words_and_bigrams_frequencies(sentences, lexicon_words)
+
+    total_words = sum([len(sentence.split()) for sentence in sentences])
+
+    for word1 in bigrams_freq:
+        for word2 in bigrams_freq[word1]:
+            prob_word1 = words_freq[word1] / total_words
+            prob_word2_given_word1 = (bigrams_freq[word1][word2] / (total_words - 1)) / prob_word1
+            bigrams_prob[word1][word2] = prob_word1 * prob_word2_given_word1
+
+    return bigrams_prob
+
+
+
+# def get_2grams_statistics(corpus, lexicon):
+#     bigrams_prob = defaultdict(float)
+#     sentences = split_text_to_sentences(corpus)
+#     lexicon_words = set(lexicon.split())
+#     lexicon_words.add('<s>')
+#     lexicon_words.add('</s>')
+#     words_freq, bigrams_freq = get_words_and_bigrams_frequencies(sentences, lexicon_words)
+#
+#     total_words = sum([len(sentence) for sentence in sentences])
+#
+#     for sentence in sentences:
+#         for i in range(1, len(sentence)):
+#             if sentence[i - 1] in lexicon_words and sentence[i] in lexicon_words:
+#                 bigram = f"{sentence[i - 1]} {sentence[i]}"
+#                 prob_word1 = words_freq[sentence[i - 1]] / total_words
+#                 prob_word2_given_word1 = (bigrams_freq[bigram] / (total_words - 1)) / prob_word1
+#                 bigrams_prob[bigram] = prob_word1 * prob_word2_given_word1
+#
+#     return bigrams_prob
+
+
+def get_max_prob_word(tokens, i, candidates, bigrams_statistics):
+    max_prob = 0
+    element = candidates.pop()
+    res = element
+    candidates.add(element)
+
+    for candidate in candidates:
+        prob = bigrams_statistics[tokens[i - 1]][candidate] * bigrams_statistics[candidate][tokens[i + 1]]
+        if prob > max_prob:
+            max_prob = prob
+            res = candidate
+
+    return res
 
 
 def solve_cloze(input, candidates, lexicon, corpus):
     # todo: implement this function
     print(f'starting to solve the cloze {input} with {candidates} using {lexicon} and {corpus}')
+    formatted_sentences = split_text_to_sentences(input)
+    if not os.path.isfile('bigrams_statistics.pkl'):
+        bigrams_statistics = get_2grams_statistics(corpus, lexicon)
+        pickle.dump(bigrams_statistics, open('bigrams_statistics.pkl', 'wb'))
+    else:
+        bigrams_statistics = pickle.load(open('bigrams_statistics.pkl', 'rb'))
+    with open(candidates, 'r', encoding='utf-8') as candidates_file:
+        candidates_text = candidates_file.read()
+    candidates = set(candidates_text.split())
 
-    return list()  # return your solution
+    solutions = []
+
+    for sentence in formatted_sentences:
+        tokens = sentence.split()
+
+        for i in range(1, len(tokens) - 1):
+            curr_token = tokens[i]
+
+            if curr_token[0] == '_':
+                pred_word = get_max_prob_word(tokens, i, candidates, bigrams_statistics)
+                candidates.remove(pred_word)
+                solutions.append(pred_word)
+
+    return solutions
 
 
 if __name__ == '__main__':
--- plot_heaps_zipf_laws.py
+++ plot_heaps_zipf_laws.py
@@ -4,29 +4,74 @@ import os.path
 from collections import defaultdict
 from matplotlib import pyplot as plt
 from math import log
+from nltk.stem import WordNetLemmatizer
+import nltk
 import seaborn as sn
 sn.set()
+nltk.download('wordnet')
 
 
-def read_data(filename):
-    word2freq = defaultdict(int)
+def read_data(filename, batch_size=100000):
+    tokens2types = defaultdict(int)
+    types = set()
+    tokens = 0
+    lemmatizer = WordNetLemmatizer()
 
-    with open(filename, 'r') as fin:
-        print('reading the text file...')
-        for i, line in enumerate(fin.readlines()):
-            for word in line.split():
-                word2freq[word] += 1
-            if i % 100000 == 0:
-                print(i)
+    with open(filename, 'r', encoding='utf-8', errors='replace') as fin:
+        print('Reading the text file...')
+        for i, line in enumerate(fin):
+            words = line.split()
+            for word in words:
+                lemma = lemmatizer.lemmatize(word, pos='v')
+                types.add(lemma)
+            tokens += len(words)
 
-    total_words = sum(word2freq.values())
-    word2nfreq = {w: word2freq[w]/total_words for w in word2freq}
+            if i % batch_size == 0 and i != 0:
+                tokens2types[tokens] = len(types)
+                print(f'Processed {i} lines')
+                # Reset the counts for the next batch
+                types.clear()
 
-    return word2nfreq
+    return tokens2types
+# def read_data(filename):
+#     word2freq = defaultdict(int)
+#
+#     with open(filename, 'r',  encoding='utf-8', errors='replace') as fin:
+#         print('reading the text file...')
+#         for i, line in enumerate(fin):
+#             for word in line.split():
+#                 word2freq[word] += 1
+#             if i % 100000 == 0:
+#                 print(i)
+#
+#     total_words = sum(word2freq.values())
+#     word2nfreq = {w: word2freq[w]/total_words for w in word2freq}
+#
+#     return word2nfreq
+
+def read_data(filename, batch_size=100000):
+    tokens2types = defaultdict(int)
+    types = set()
+    tokens = 0
+    lemmatizer = WordNetLemmatizer()
+
+    with open(filename, 'r', encoding='utf-8', errors='replace') as fin:
+        print('Reading the text file...')
+        for i, line in enumerate(fin):
+            words = line.split()
+            for word in words:
+                lemma = lemmatizer.lemmatize(word, pos='v')
+                types.add(lemma)
+            tokens += len(words)
+
+            if i % batch_size == 0 and i != 0:
+                tokens2types[tokens] = len(types)
+                print(f'Processed {i} lines')
+    return tokens2types
 
 
-def plot_zipf_law(word2nfreq):
-    y = sorted(word2nfreq.values(), reverse=True)
+def plot_zipf_law(tokens2types):
+    y = sorted(tokens2types.values(), reverse=True)
     x = list(range(1, len(y)+1))
 
     product = [a * b for a, b in zip(x, y)]
@@ -41,14 +86,23 @@ def plot_zipf_law(word2nfreq):
     plt.title("Zipf's law")
     plt.show()
 
+def plot_heap_law(tokens2types):
+    y = list(tokens2types.values())
+    x = list(tokens2types.keys())
+
+    plt.plot(x, y)
+    plt.xlabel('Tokens')
+    plt.ylabel('Types')
+    plt.title("Heap's Law")
+    plt.show()
 
 if __name__ == '__main__':
     with open('config.json', 'r') as json_file:
         config = json.load(json_file)
 
-    if not os.path.isfile('word2nfreq.pkl'):
+    if not os.path.isfile('tokens2types.pkl'):
         data = read_data(config['corpus'])
-        pickle.dump(data, open('word2nfreq.pkl', 'wb'))
+        pickle.dump(data, open('tokens2types.pkl', 'wb'))
 
-    plot_zipf_law(pickle.load(open('word2nfreq.pkl', 'rb')))
+    plot_heap_law(pickle.load(open('tokens2types.pkl', 'rb')))
 
